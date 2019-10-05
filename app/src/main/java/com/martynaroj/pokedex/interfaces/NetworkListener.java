ef811dc0b39ca6f75420091372465340971650fc
Martyna Roj <martynaxroj@gmail.com>
2019-10-05 18:06:25 +0200

    Add default method checking internet connection

--- app/src/main/java/com/martynaroj/pokedex/interfaces/NetworkListener.java
+++ app/src/main/java/com/martynaroj/pokedex/interfaces/NetworkListener.java
@@ -1,7 +1,15 @@
 package com.martynaroj.pokedex.interfaces;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+
 public interface NetworkListener {
 
     void showNetworkAlert();
+
+    default boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
 
 }
